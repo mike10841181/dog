742e669463d4cdf8dbf51faa418777c334a4dc60
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2011-03-23 22:49:00 -0600

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogs::Application.config.secret_token = 'ee0345b4cc14188765a0f1e1c20aaf75387bbcc15a5aea65d44fc98e21a155d6bb0c2aedd22321e0f23ad1a691e61b0f0a89f6acf5d662afd04232f317f016d7'
